3ded2c0d524b06b115c5768b5cf37dec3d65fe42
Israel Guido <israel.guido@e-smart.com.br>
2014-04-07 18:13:41 -0300

    Manual de uso e contatos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 mysql-bkp-shell
 ===============
+
+Script para backup de banco de dados REMOTO ou LOCAL.
+Author: Israel Guido
+E-mail: eu@israelguido.com.br
+Date: 2014-04
+Website: http://israelguido.com.br
---
+++ dumps.sh
@@ -0,0 +1,37 @@
+#
+# script by israelguido@gmail.com
+#
+
+echo "Digite seu dns";
+read host;
+echo "Digite seu user";
+read user;
+echo "Digite seu pass";
+stty -echo
+read pass;
+echo "Digite a base que deseja executar o backup";
+stty echo
+read base;
+echo "Voce deseja [I]mportar ou [E]xportar";
+read typi;
+if [ $typi = "E" ]
+then 
+	mysqldump -h $host -u $user -p$pass $base > $base"_backup.sql";
+	if [ -e $base"_backup.sql" ]
+	then
+		echo "Seu arquivo foi criado com sucesso em: " `pwd`"/"$base"_backup.sql";
+	else
+		echo "Houve algum problema";
+	fi
+else
+	echo "Digite o caminho completo do seu aquivo SQL";
+	read filename;
+	if [ -e $filename ] 
+	then
+		mysql -h $host -u $user -p$pass $base < $filename; 
+		echo "Import efetuado com sucesso";
+	fi
+	
+fi
+
+
