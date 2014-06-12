54b16f25be39619e578d8693f7ce10e4e52c33d4
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-06-12 02:33:54 -0300

    Alteração nlógica na semântica shell

--- dumps.sh
+++ dumps.sh
@@ -2,34 +2,34 @@
 # script by israelguido@gmail.com
 #
 
-echo "Digite seu dns";
-read host;
-echo "Digite seu user";
-read user;
-echo "Digite seu pass";
+echo "Digite seu dns"
+read host
+echo "Digite seu user"
+read user
+echo "Digite seu pass"
 stty -echo
-read pass;
-echo "Digite a base que deseja executar o backup";
+read pass
+echo "Digite a base que deseja executar o backup"
 stty echo
-read base;
-echo "Voce deseja [I]mportar ou [E]xportar";
-read typi;
+read base
+echo "Voce deseja [I]mportar ou [E]xportar"
+read typi
 if [ $typi = "E" ]
 then 
-	mysqldump -h $host -u $user -p$pass $base > $base"_backup.sql";
+	mysqldump -h $host -u $user -p$pass $base > $base"_backup.sql"
 	if [ -e $base"_backup.sql" ]
 	then
-		echo "Seu arquivo foi criado com sucesso em: " `pwd`"/"$base"_backup.sql";
+		echo "Seu arquivo foi criado com sucesso em: " `pwd`"/"$base"_backup.sql"
 	else
-		echo "Houve algum problema";
+		echo "Houve algum problema"
 	fi
 else
-	echo "Digite o caminho completo do seu aquivo SQL";
-	read filename;
+	echo "Digite o caminho completo do seu aquivo SQL"
+	read filename
 	if [ -e $filename ] 
 	then
-		mysql -h $host -u $user -p$pass $base < $filename; 
-		echo "Import efetuado com sucesso";
+		mysql -h $host -u $user -p$pass $base < $filename
+		echo "Import efetuado com sucesso"
 	fi
 	
 fi
